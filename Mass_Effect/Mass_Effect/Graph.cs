fd0e19a899dd5ee91cfe5208a1c142d74af9e366
Szabolcs Kun <17924472+szabolcskun98@users.noreply.github.com>
2019-04-22 11:16:57 +0200

    LinkedList class rewrite, Graph implementation

---
+++ Mass_Effect/Mass_Effect/Graph.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mass_Effect
+{
+    enum SearchType
+    {
+        DFS, //Depth-first search
+        BFS //Breadth-first search
+    }
+    class GraphNode<T>
+    {
+        private T value;
+        private List<GraphNode<T>> neighbors;
+
+        public GraphNode(T value)
+        {
+            this.value = value;
+            this.neighbors = new List<GraphNode<T>>();
+        }
+
+        public T Value
+        {
+            get { return value; }
+        }
+
+        public IList<GraphNode<T>> Neighbors
+        {
+            get { return neighbors.AsReadOnly(); }
+        }
+
+        public bool addNeighbor(GraphNode<T> neighbor)
+        {
+            if (neighbors.Contains(neighbor))
+            {
+                return false;
+            }
+            else
+            {
+                neighbors.Add(neighbor);
+                return true;
+            }
+        }
+
+        public bool removeNeighbor(GraphNode<T> neighbor)
+        {
+            return neighbors.Remove(neighbor);
+        }
+
+        public bool removeAllNeighbors()
+        {
+            for (int i = neighbors.Count - 1; i >= 0; i--)
+            {
+                neighbors.RemoveAt(i);
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder nodeString = new StringBuilder();
+            nodeString.Append($"[Node value: {value}, Neighbors: ");
+            for (int i = 0; i < neighbors.Count; i++)
+            {
+                nodeString.Append($"{neighbors[i].value} ");
+            }
+            nodeString.Append("]");
+            return nodeString.ToString();
+        }
+    }
+    class Graph<T>
+    {
+        List<GraphNode<T>> nodes = new List<GraphNode<T>>();
+
+        public Graph()
+        {
+        }
+
+        public int Count
+        {
+            get { return nodes.Count; }
+        }
+
+        public IList<GraphNode<T>> Nodes
+        {
+            get { return nodes.AsReadOnly(); }
+        }
+
+        public void Clear()
+        {
+            foreach (GraphNode<T> node in nodes)
+            {
+                node.removeAllNeighbors();
+            }
+
+            for (int i = nodes.Count - 1; i >= 0; i--)
+            {
+                nodes.RemoveAt(i);
+            }
+        }
+
+        public GraphNode<T> find(T value)
+        {
+            foreach (GraphNode<T> node in nodes)
+            {
+                if (node.Value.Equals(value)) return node;
+            }
+            return null;
+        }
+
+        public bool addNode(T value)
+        {
+            if (find(value) != null)
+            {
+                return false;
+            }
+            else
+            {
+                nodes.Add(new GraphNode<T>(value));
+                return true;
+            }
+        }
+
+        public bool addEdge(T value1, T value2)
+        {
+            GraphNode<T> node1 = find(value1);
+            GraphNode<T> node2 = find(value2);
+            if (node1 == null || node2 == null) return false;
+            else if (node1.Neighbors.Contains(node2)) return false;
+            else
+            {
+                node1.addNeighbor(node2);
+                node2.addNeighbor(node1);
+                return true;
+            }
+        }
+
+        public bool removeNode(T value)
+        {
+            GraphNode<T> removeNode = find(value);
+            if (removeNode == null) return false;
+            else
+            {
+                nodes.Remove(removeNode);
+                foreach (GraphNode<T> node in nodes)
+                {
+                    node.removeNeighbor(removeNode);
+                }
+                return true;
+            }
+        }
+
+        public bool removeEdge(T value1, T value2)
+        {
+            GraphNode<T> node1 = find(value1);
+            GraphNode<T> node2 = find(value2);
+            if (node1 == null || node2 == null) return false;
+            else if (!node1.Neighbors.Contains(node2)) return false;
+            else
+            {
+                node1.removeNeighbor(node2);
+                node2.removeNeighbor(node1);
+                return true;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < Count; i++)
+            {
+                builder.Append(nodes[i].ToString());
+                if (i < Count - 1) builder.Append(",");
+            }
+            return builder.ToString();
+        }
+    }
+
+    class PathNodeInfo<T>
+    {
+        GraphNode<T> previous;
+
+        public PathNodeInfo(GraphNode<T> previous)
+        {
+            this.previous = previous;
+        }
+
+        public GraphNode<T> Previous
+        {
+            get { return previous; }
+        }
+
+    }
+}
---
+++ Mass_Effect/Mass_Effect/LinkedList.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mass_Effect
+{
+    class LinkedListNode<T>
+    {
+        T value;
+        LinkedListNode<T> next;
+        public LinkedListNode(T value, LinkedListNode<T> next)
+        {
+            this.value = value;
+            this.next = next;
+        }
+
+        public T Value
+        {
+            get { return value; }
+        }
+
+        public LinkedListNode<T> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+    }
+
+    abstract class LinkedList<T> : IEnumerable<T>
+    {
+        protected LinkedListNode<T> head;
+        protected int count;
+
+        protected LinkedList()
+        {
+            this.head = null;
+            this.count = 0;
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public LinkedListNode<T> Head
+        {
+            get { return head; }
+        }
+
+        public abstract void Add(T item);
+
+        public void Clear()
+        {
+            if (head != null)
+            {
+                LinkedListNode<T> previousNode = head;
+                LinkedListNode<T> currentNode = head.Next;
+                previousNode.Next = null;
+                while (currentNode != null)
+                {
+                    previousNode = currentNode;
+                    currentNode = currentNode.Next;
+                    previousNode.Next = null;
+                }
+            }
+
+            head = null;
+            count = 0;
+        }
+        public bool Remove(T item)
+        {
+            if (head == null) return false;
+            else if (head.Value.Equals(item))
+            {
+                head = head.Next;
+                count--;
+                return true;
+            }
+            else
+            {
+                LinkedListNode<T> previousNode = head;
+                LinkedListNode<T> currentNode = head.Next;
+                while (currentNode != null && !currentNode.Value.Equals(item))
+                {
+                    previousNode = currentNode;
+                    currentNode = currentNode.Next;
+                }
+
+                if (currentNode == null) return false;
+                else
+                {
+                    previousNode.Next = currentNode.Next;
+                    count--;
+                    return true;
+                }
+            }
+        }
+        public LinkedListNode<T> Find(T item)
+        {
+            LinkedListNode<T> currentNode = head;
+            while (currentNode != null && !currentNode.Value.Equals(item))
+            {
+                currentNode = currentNode.Next;
+            }
+
+            if (currentNode != null) return currentNode;
+            else return null;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            LinkedListNode<T> currentNode = head;
+            int nodeCount = 0;
+            builder.Append("[");
+            while (currentNode != null)
+            {
+                nodeCount++;
+                builder.Append(currentNode.Value.ToString());
+                if (nodeCount < count)
+                {
+                    builder.Append(",");
+                }
+                currentNode = currentNode.Next;
+            }
+            builder.Append("]");
+            return builder.ToString();
+        }
+
+        public delegate void Operation(T item);
+        public void Traverse(Operation o)
+        {
+            LinkedListNode<T> node = head;
+            while (node != null)
+            {
+                o(node.Value);
+                node = node.Next;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new ListEnumerator(head);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        class ListEnumerator : IEnumerator<T>
+        {
+            LinkedListNode<T> node;
+            LinkedListNode<T> cursor;
+
+            public ListEnumerator(LinkedListNode<T> node)
+            {
+                this.node = node;
+                this.cursor = null;
+            }
+
+            public T Current => cursor.Value;
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+                node = null;
+                cursor = null;
+            }
+
+            public bool MoveNext()
+            {
+                if (cursor == null)
+                {
+                    cursor = node;
+                }
+                else
+                {
+                    cursor = cursor.Next;
+                }
+                return cursor != null;
+            }
+
+            public void Reset()
+            {
+                cursor = null;
+            }
+        }
+    }
+
+    class UnsortedLinkedList<T> : LinkedList<T>
+    {
+        public UnsortedLinkedList() : base()
+        {
+        }
+
+        public override void Add(T item)
+        {
+            if (head == null) head = new LinkedListNode<T>(item, null);
+            else head = new LinkedListNode<T>(item, head);
+            count++;
+        }
+    }
+
+    class SortedLinkedList<T> : LinkedList<T> where T : IComparable
+    {
+        public SortedLinkedList() : base()
+        {
+        }
+
+        public override void Add(T item)
+        {
+            if (head == null) head = new LinkedListNode<T>(item, null);
+            else if (head.Value.CompareTo(item) > 0) head = new LinkedListNode<T>(item, head);
+            else
+            {
+                LinkedListNode<T> previousNode = null;
+                LinkedListNode<T> currentNode = head;
+                while (currentNode != null && currentNode.Value.CompareTo(item) < 0)
+                {
+                    previousNode = currentNode;
+                    currentNode = currentNode.Next;
+                }
+                previousNode.Next = new LinkedListNode<T>(item, currentNode);
+            }
+            count++;
+        }
+    }
+}
