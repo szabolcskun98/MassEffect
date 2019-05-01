f1a46e0beb3bdedfba350afd9e5067c78a1f9731
Szabolcs Kun <17924472+szabolcskun98@users.noreply.github.com>
2019-05-01 23:01:34 +0200

    General game classes

---
+++ Mass_Effect/Mass_Effect/Mission.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mass_Effect
+{
+    class Mission
+    {
+        string name;
+        string details;
+        int credit;
+        int riskLevel;
+
+        public Mission(string name, string details, int credit, int riskLevel)
+        {
+            this.name = name;
+            this.details = details;
+            this.credit = credit;
+            this.riskLevel = riskLevel;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Details { get => details; set => details = value; }
+        public int Credit { get => credit; set => credit = value; }
+        public int RiskLevel { get => riskLevel; set => riskLevel = value; }
+
+        public int Weight
+        {
+            get
+            {
+                return credit / riskLevel;
+            }
+        }
+    }
+}
---
+++ Mass_Effect/Mass_Effect/StellarSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mass_Effect
+{
+    class StellarSystem
+    {
+        string name;
+        SingleLinkedList<Mission> missions;
+
+        public StellarSystem(string name, SingleLinkedList<Mission> missions)
+        {
+            this.name = name;
+            this.missions = missions;
+        }
+
+        public string Name { get => name; set => name = value; }
+        internal SingleLinkedList<Mission> Missions { get => missions; set => missions = value; }
+
+        public int LenghtOfMissions()
+        {
+            return Missions.Count();
+        }
+
+        public double SumOfWeight()
+        {
+            return Missions.Sum(x => x.Weight);
+        }
+    }
+}
